bc2754757ce35841763f90adc19b65e223ecb942
kim <kim.gunjevic@gmail.com>
2023-01-23 17:20:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+var pravokutnici= document.getElementsByClassName ("pravokutnik")
+console.log (pravokutnici);
+
+for (var i=0; i < pravokutnici.lenght; i++)
+{ 
+    pravokutnici[i].style.backgroundColor = prompt();
+
+}
